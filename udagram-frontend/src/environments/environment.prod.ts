eca65244e8980e3d075abb8659af2c289f104a99
kelue <austincmoon5@gmail.com>
2021-10-02 14:57:39 +0100

    updated frontend external IP

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a4d7735bb75944ed9a9d4efd664b79ba-633851532.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
